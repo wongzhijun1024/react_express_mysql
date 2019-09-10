efbcd1c4539e82b8098229d1c323a02f08b895f2
炽天使 <qc_481763d804b845ae9ed45fda42800afc@git.code.tencent.com>
2019-09-10 10:45:03 +0800

    add the code

--- 01client/01wx/pages/write/write.js
+++ 01client/01wx/pages/write/write.js
@@ -1,66 +1,76 @@
-// pages/write/write.js
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    height: '500',
+    coursesIndex: 0,
+    courses: []
+  },
+  onLoad: function () {
+    let that = this;
+    //获得后台服务的数据
+    wx.request({
+      url: 'http://localhost:8888/index',
+      success: function (res) {
+        if (res.statusCode == 200) {
+          let courses = res.data;
+
+          for (let i = 0; i < courses.length;i++){
+            for (let j = 0; j < courses[i].chapters.length;j++){
+              courses[i].chapters[j].isSwitch=false;
+              }
+          }
+          that.setData({
+            courses: courses
+          });
+
+          //把当前的课程保存为全局变量
+          app.globalData.courses = courses;
+
+        }
+      }
+    })
+    //获得屏幕的高度
+    let height = app.globalData.windowHeight;
+    this.setData({
+      height: height - 90
+    });
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  clickTopItem: function (res) {
+    //获得传递过来的数据
+    let id = res.currentTarget.dataset.yjs;
+    //把index换成id
+    this.setData({
+      coursesIndex: id
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toExaminationInfor: function () {
+    wx.navigateTo({
+      url: '../examinationInfor/examinationInfor',
+    })
+  }
+  , showChapterList: function (event) {
+    //课程的下标
+    let coursesIndex = this.data.coursesIndex;
+    //第几个章节 
+    let chapterIndex = event.currentTarget.dataset.chapterindex;
+    //获得课程数据
+    let courses = this.data.courses;
+    //获得当前课程选择的题库列表
+    let isSwitch = courses[coursesIndex].chapters[chapterIndex].isSwitch;
+    //如果当前被打开，那么就关闭
+    if (isSwitch){
+      isSwitch=false;
+    }else{
+      isSwitch=true;
+    }
+    //把改变的值重新给课程数据设置
+    courses[coursesIndex].chapters[chapterIndex].isSwitch = isSwitch;
+    this.setData({
+      courses: courses
+    })
   }
 })
